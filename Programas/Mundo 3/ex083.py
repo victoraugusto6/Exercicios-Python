5c67c1f8f979a1742583bd5c5e2481496da5f10f
Victor Augusto <victor.augusto60@gmail.com>
2020-12-28 17:25:19 -0300

    commit

--- Programas/Mundo 3/ex083.py
+++ Programas/Mundo 3/ex083.py
@@ -1,9 +1,16 @@
-exp = []
-exp = input('Digite a expressão: ')
-pa = exp.count('(')
-pf = exp.count(')')
+exp = str(input('Digite a expressão: '))
+cont = 0
 
-if(pa == pf):
-    print(f'A expressão {exp} é válida')
+for x in exp:
+    if x == '(':
+        cont += 1
+    elif x == ')':
+        cont -= 1
+        if cont < 0:  # Fechou antes de abrir
+            print('Expressao inválida')
+            exit(0)  # Termina programa
+
+if cont != 0:  # Tem parênteses sem seu par
+    print('Expressao inválida')
 else:
-    print(f'A expressão {exp} não é válida')
+    print('Expressao válida')
