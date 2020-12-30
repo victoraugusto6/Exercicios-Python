ecfffcf89885b65ac30a856844e2c60ac1192227
Victor Augusto <victor.augusto60@gmail.com>
2020-12-30 17:33:38 -0300

    commit

--- Programas/Mundo 3/modulos/numeros.py
+++
@@ -1,7 +0,0 @@
-from uteis import fatorial,dobro,triplo
-
-num = int(input('Digite um valor: '))
-fat = fatorial(num)
-print(f'O fatorial de {num} é {fat}')
-print(f'O dobro de {num} é {dobro(num)}')
-print(f'O Triplo de {num} é {triplo(num)}')
